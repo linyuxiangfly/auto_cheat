6bbd87ef637fc0068b8c0cea77ed7a8cabd4fe88
linyuxiangfly <280789588@qq.com>
2021-11-18 19:20:14 +0800

    消耗CPU
    计算CPU使用率是不是很低，如果低就消耗CPU

--- script/cheat.sh
+++ script/cheat.sh
@@ -11,7 +11,26 @@ do
   mem=$mem_used_persent
 
   #echo "cpu:$cpu,mem:$mem"
-  echo "cpu:$cpu,mem:$mem" >> `dirname $0`/1.txt
+  #echo "cpu:$cpu,mem:$mem" >> `dirname $0`/1.txt
+
+  #计算CPU使用率是不是很低
+  cpuIsLow=`echo "if($cpu < 20.0) print "1" else print "0"" | bc`
+
+  if [ $cpuIsLow -eq 1 ]; then
+    echo "low" >> `dirname $0`/1.txt
+
+    #消耗CPU，后面加上&是让命令在后台执行
+    timeout 1s cat /dev/urandom |gzip -9|gzip -d |gzip -9 |gzip -d>/dev/null &
+
+    #让CPU占满CPU
+    #for i in `seq 1 $(cat /proc/cpuinfo |grep "physical id" |wc -l)`; do dd if=/dev/zero of=/dev/null & done
+    #杀死dd进程
+    #pkill -9 dd
+    #sleep 1
+  else
+    echo "cpu:$cpu,mem:$mem" >> `dirname $0`/1.txt
+  fi
 
   sleep 1
+
 done
