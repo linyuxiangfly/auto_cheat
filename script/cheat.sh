56169275463f10bf379a0030fe38088c469ae4ca
linyuxiangfly <280789588@qq.com>
2021-11-18 21:53:06 +0800

    增加内存占用
    增加内存占用

--- script/cheat.sh
+++ script/cheat.sh
@@ -14,10 +14,10 @@ do
   #echo "cpu:$cpu,mem:$mem" >> `dirname $0`/1.txt
 
   #计算CPU使用率是不是很低
-  cpuIsLow=`echo "if($cpu < 20.0) print "1" else print "0"" | bc`
+  cpuIsLow=`echo "if($cpu < 5.0) print "1" else print "0"" | bc`
 
   if [ $cpuIsLow -eq 1 ]; then
-    echo "low" >> `dirname $0`/1.txt
+    echo "cpu low" >> `dirname $0`/1.txt
 
     #消耗CPU，后面加上&是让命令在后台执行
     timeout 1s cat /dev/urandom |gzip -9|gzip -d |gzip -9 |gzip -d>/dev/null &
@@ -27,6 +27,18 @@ do
     #杀死dd进程
     #pkill -9 dd
     #sleep 1
+  else
+    echo "cpu:$cpu,mem:$mem" >> `dirname $0`/1.txt
+  fi
+
+  #计算内存使用率是不是很低
+  memIsLow=`echo "if($mem < 20.0) print "1" else print "0"" | bc`
+  if [ $memIsLow -eq 1 ]; then
+    echo "mem low" >> `dirname $0`/1.txt
+
+    # $1 内存,比如100M
+    # $2 占用内存时间（s）
+    ./consume_mem.sh 100 1 &
   else
     echo "cpu:$cpu,mem:$mem" >> `dirname $0`/1.txt
   fi
---
+++ script/consume_mem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#free -m > /tmp/freee
+#cat /tmp/freee
+mkdir /tmp/$3
+mount -t tmpfs -o size=$1M tmpfs /tmp/$3
+dd if=/dev/zero of=/tmp/$3/block bs=1048576 count=$1
+#free -m > /tmp/freee
+#cat /tmp/freee
+sleep $2
+rm -rf /tmp/$3/block
+umount /tmp/$3
+rmdir /tmp/$3
